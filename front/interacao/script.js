e105dc2d56a2d254da6a4d3b047550c5f8f01cb9
Marcothulio1207 <marcothulio120799@gmail.com>
2024-11-04 23:33:11 -0300

    Tela login javalirica

---
+++ front/interacao/script.js
@@ -0,0 +1,21 @@
+const predefinidoEmail01 = "user@gmail.com";
+const predefinidosenha01 = "123";
+
+
+function login(){
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if(!email || !password){
+        alert("Por favor, preencha todos os campos")
+        return;
+    }
+
+    if (email == predefinidoEmail01  && password == predefinidosenha01){
+        alert("Login efetuado com sucesso!")
+        // window.location.href = "home.html"
+    }
+    else {
+        alert("Email ou senha incorretos!")
+    }
+}
